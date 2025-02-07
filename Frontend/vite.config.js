2111b9aba814601ea231b75ad486481a107c8b5a
Satish Kumar <satishvaishna@gmail.com>
2025-02-07 18:24:30 +0530

    Fix: Externalize lucide-react, react-icons, and framer-motion in Rollup

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
   build: {
     rollupOptions: {
-      external: ["framer-motion"],
+      external: ["framer-motion", "lucide-react", "react-icons"]
     },
   },
 });
