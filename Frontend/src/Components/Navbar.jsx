b63ea3494b8f2680ad39c501f8a7d0f13a80e51f
Satish Kumar <satishvaishna@gmail.com>
2025-02-18 15:24:02 +0530

    remove bg image

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="fixed top-4 right-4 left-4 rounded-xl p-4 bg-white/10 backdrop-blur-lg shadow-lg">
+    <nav className="fixed top-4 right-4 left-4 rounded-xl p-4 bg-amber-100 backdrop-blur-lg shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-bold">Satish Portfolio</div>
-        <div className="hidden md:flex space-x-6">
-          <a href="#home" className="text-white hover:text-gray-300">Home</a>
-          <a href="#about" className="text-white hover:text-gray-300">About</a>
-          <a href="#skills" className="text-white hover:text-gray-300">Skills</a>
-          <a href="#projects" className="text-white hover:text-gray-300">Projects</a>
-          <a href="#contact" className="text-white hover:text-gray-300">Contact</a>
+        <div className="text-gray-900 text-2xl font-bold">Satish Portfolio</div>
+        <div className="hidden md:flex space-x-6 text-black">
+          <a href="#home" className=" hover:text-gray-500">Home</a>
+          <a href="#about" className=" hover:text-gray-500">About</a>
+          <a href="#skills" className=" hover:text-gray-500">Skills</a>
+          <a href="#projects" className=" hover:text-gray-500">Projects</a>
+          <a href="#contact" className=" hover:text-gray-500">Contact</a>
         </div>
         <div className="md:hidden">
           <button onClick={() => setIsOpen(!isOpen)} className="text-white">
