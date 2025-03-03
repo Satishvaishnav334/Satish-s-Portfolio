308702cdac4cc0c108369224fa92b964090af17f
Satish Kumar <satishvaishna@gmail.com>
2025-03-03 18:37:45 +0530

    try to fix cors error

--- server/app.js
+++ server/app.js
@@ -5,23 +5,7 @@ import contactRoute from "./routes/contactRoute.js";
 
 const app = express();
 
-app.use(cors({
-  origin:'https://www.satishvaishnav.in/'
-}))
-// ✅ Set CORS Configuration
-const corsOptions = {
-  origin: "https://www.satishvaishnav.in", // Allow frontend domain
-  credentials: true, // Allow cookies, authorization headers
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  optionsSuccessStatus: 200 // Preflight request success status
-};
 
-// ✅ Apply CORS Middleware (Only Once)
-app.use(cors(corsOptions));
-
-// ✅ Handle Preflight Requests
-app.options("*", cors(corsOptions)); // Handles preflight requests properly
 
 // ✅ Middleware for Parsing Request Body
 app.use(express.json({ limit: "16kb" }));
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import app from "./app.js";
+import cors from 'cors'
 
 const port = process.env.PORT || 8000;
 dotenv.config({ path: './.env' });
-
+app.use(cors({
+  origin:'https://www.satishvaishnav.in'
+}))
 connectDB().then(() => {
   app.listen(port, () => console.log(`Server running on port ${port}`));
 }).catch((error) => {
