acbecd9c1b3de645cf896bbd4ca539bde1f684b1
Satish Kumar <satishvaishna@gmail.com>
2025-04-20 00:08:24 +0530

    mix

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,51 +1,18 @@
 // Navbar.jsx
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="fixed left-1/2 transform -translate-x-1/2 w-[25vw] bg-cyan-200/20 backdrop-blur-md border border-cyan-100/30 h-15 mt-5 p-2 rounded-3xl z-50 shadow-lg">
+    <header className="fixed md:left-1/2 left-1/2  transform -translate-x-1/2 w-[80vw] bg-cyan-200/20 backdrop-blur-md border border-cyan-100/30 h-15 mt-5 p-2 rounded-3xl z-50 shadow-lg">
       {/* Mobile Menu Toggle */}
-      <div className="md:hidden flex justify-between items-center">
-        <div className="text-white font-semibold text-lg">Menu</div>
-        <button onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? <X className="text-white" /> : <Menu className="text-white" />}
-        </button>
-
-        {/* Mobile Dropdown Menu */}
-        <AnimatePresence>
-          {isOpen && (
-            <motion.nav
-              initial={{ height: 0 }}
-              animate={{ height: "auto" }}
-              exit={{ height: 0 }}
-              className="absolute top-full left-0 w-full bg-cyan-200/20 backdrop-blur-lg mt-2 rounded-b-3xl shadow-md"
-            >
-              <ul className="flex flex-col items-center text-white py-4 space-y-4">
-                <motion.li initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }}>
-                  <a href="/" onClick={() => setIsOpen(false)}>Home</a>
-                </motion.li>
-                <motion.li initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }}>
-                  <a href="/skills" onClick={() => setIsOpen(false)}>Skills</a>
-                </motion.li>
-                <motion.li initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }}>
-                  <a href="/project" onClick={() => setIsOpen(false)}>Projects</a>
-                </motion.li>
-                <motion.li initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }}>
-                  <a href="/contact" onClick={() => setIsOpen(false)}>Contact</a>
-                </motion.li>
-              </ul>
-            </motion.nav>
-          )}
-        </AnimatePresence>
-      </div>
+      
 
       {/* Desktop Menu */}
-      <nav className="hidden md:flex justify-center mt-2">
-        <ul className="flex space-x-10 text-white text-lg">
+      <nav className=" md:flex justify-center mt-2">
+        <ul className="flex gap-4 pl-2 text-white text-lg">
           <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
             <a href="/">Home</a>
           </motion.li>
