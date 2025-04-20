079ec3cb71e39c49a9d3ab1b05485a62c6a93730
Satish Kumar <satishvaishna@gmail.com>
2025-04-20 11:25:01 +0530

    responsive

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="fixed md:left-1/2 left-1/2  transform -translate-x-1/2 w-[80vw] bg-cyan-200/20 backdrop-blur-md border border-cyan-100/30 h-15 mt-5 p-2 rounded-3xl z-50 shadow-lg">
+    <header className="fixed md:left-1/2 left-1/2  transform -translate-x-1/2 w-[85vw] bg-cyan-200/20 backdrop-blur-md border border-cyan-100/30 h-15 mt-5 p-2 rounded-3xl z-50 shadow-lg">
       {/* Mobile Menu Toggle */}
       
 
       {/* Desktop Menu */}
       <nav className=" md:flex justify-center mt-2">
-        <ul className="flex gap-4 pl-2 text-white text-lg">
+        <ul className="flex gap-3 justify-center text-white text-lg">
           <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
             <a href="/">Home</a>
           </motion.li>
