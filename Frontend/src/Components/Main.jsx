103ed71c159f7d31f44c8c8a5e42141ced17a394
Satish Kumar <satishvaishna@gmail.com>
2025-04-20 12:49:38 +0530

    updat

--- Frontend/src/Components/Main.jsx
+++ Frontend/src/Components/Main.jsx
@@ -15,7 +15,7 @@ const Main = () => {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.3, duration: 0.6 }}
-          className="text-3xl sm:text-4xl md:text-6xl font-bold text-black mb-4"
+          className="text-xl sm:text-xl md:text-2xl font-bold text-black mb-4"
         >
           Hi, I'm <span className="text-gray-600">Satish Vaishnav</span>
         </motion.h1>
